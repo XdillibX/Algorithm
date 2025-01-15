e7ae61cda49735f4906943efb00c473450782656
XdillibX <142654156+XdillibX@users.noreply.github.com>
2025-01-15 23:00:32 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+	int r1, r2, r3, r4;
+
+	scanf("%d %d %d", &a, &b, &c);
+	
+
+	r1 = (a + b) % c;
+	r2 = ((a % c) + (b % c)) % c;
+	r3 = (a * b) % c;
+	r4 = ((a % c) *(b % c)) % c;
+
+	printf("%d \n", r1);
+	printf("%d \n", r2);
+	printf("%d \n", r3);
+	printf("%d \n", r4);
+
+	return 0;
+
+
+
+}
