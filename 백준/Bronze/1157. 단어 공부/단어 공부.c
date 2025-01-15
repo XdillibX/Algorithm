09fae0c284e2c58d35ad13075406a4a473470ffa
XdillibX <142654156+XdillibX@users.noreply.github.com>
2025-01-15 22:33:48 +0900

    [Bronze I] Title: 단어 공부, Time: 48 ms, Memory: 9904 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.c
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+char msg[1000001];
+char ans;
+
+int count = 0;
+int ansNum = 1;
+int aNum = 0;
+int imsg[1000001] = { 0, };
+int cnt[1000001] = { 0, };
+int i1, i2, i3, i4;
+int max, maxNum = 0;
+
+int main()
+{
+   
+
+   
+
+    scanf("%s", msg);
+
+    while (msg[count] != '\0')
+        count++;
+
+    for (int i = 0; i < count; i++)
+    {
+        if (msg[i] >= 'a' && msg[i] <= 'z') msg[i] -= 32;
+    }
+
+    for (int i = 0; i < count; i++) imsg[i] = (int)msg[i];
+
+    for (i1 = 0; i1 < count; i1++) {
+        for (i2 = 0; i2 < i1; i2++) {
+            if (imsg[i1] == imsg[i2]) {
+                cnt[i2]++;
+                break;
+            }
+        }
+
+        if (i2 == i1) cnt[i2]++;
+    }
+
+
+    max = cnt[0];
+    for (i3 = 0; i3 < count; i3++) {
+
+        if (max < cnt[i3])
+        {
+            max = cnt[i3];
+            maxNum = i3;
+        }
+
+    }
+
+    for (i4 = 0; i4 < count; i4++)
+    {
+        if (max == cnt[i4])
+        {
+            aNum++;
+        }
+    }
+
+    if (aNum == 1)
+    {
+        ansNum = 1;
+    }
+    else
+    {
+        ansNum = 2;
+    }
+
+    if (ansNum == 1)
+    {
+        printf("%c", (char)imsg[maxNum]);
+    }
+    else
+    {
+        printf("?");
+    }
+
+
+
+    return 0;
+}
