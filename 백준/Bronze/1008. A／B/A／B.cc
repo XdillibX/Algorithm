1c3378ab8510a51e67dff8d96f60faea6dff70f5
XdillibX <142654156+XdillibX@users.noreply.github.com>
2025-01-15 22:32:19 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+	
+
+	
+	printf("%.9f \n", (double)a/(double)b);
+
+	return 0;
+
+
+
+}
