5131d60a975f332b565d8cad822508aeae1b721e
XdillibX <142654156+XdillibX@users.noreply.github.com>
2025-01-17 02:40:19 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.c
+++ 백준/Bronze/15829. Hashing/Hashing.c
@@ -2,22 +2,18 @@
 
 int main(void) {
     int n;
-    int tmp;
-    long ans = 0;
+    long long tmp = 1;
+    long long ans = 0;
     char str[51];
     
-    scanf("%d", &n);
+    scanf("%d %s", &n, str);
     
     for (int i = 0; i < n; i++) {
-        scanf(" %c", &str[i]);
-        tmp = 1;
-        for (int j = 0; j < i; j++) {
-            tmp *= 31;
-        }
-        ans += (int)(str[i] - 'a' + 1) * tmp;
+        ans = (ans + (str[i] - 'a' + 1) * tmp) % 1234567891;
+        tmp = (tmp * 31) % 1234567891;
     }
     
-    printf("%ld", ans % 1234567891);
+    printf("%lld", ans);
     
     return 0;
 }
