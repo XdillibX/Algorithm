a9e7f23b92c8005ae1e861cd9a034230eecda6d2
XdillibX <142654156+XdillibX@users.noreply.github.com>
2025-01-20 16:03:26 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.c
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(void) {
     int N;
-    long sum = 2;
+    int length = 2;
     
     scanf("%d", &N);
     
     for (int i = 0; i < N; i++) {
-        sum += pow(2,i);
+        length += length - 1;
     }
     
-    printf("%.f", pow(sum,2));
+    printf("%d", length * length);
     
     return 0;
 }
