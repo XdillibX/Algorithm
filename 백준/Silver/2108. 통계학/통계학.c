800fe3d2b4ec1c3a7cf458ffca9854b2a2fd0842
XdillibX <142654156+XdillibX@users.noreply.github.com>
2025-01-20 13:50:41 +0900

    [Silver III] Title: 통계학, Time: 132 ms, Memory: 4904 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.c
+++ 백준/Silver/2108. 통계학/통계학.c
@@ -1,105 +1,86 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-int compare(const void* first, const void* second)
-{
-	int* a = (int*)first;
-	int* b = (int*)second;
-
-	if (*a < *b)
-		return -1;
-	else if (*a > *b)
-		return 1;
-	else
-		return 0;
+int compare(const void* a, const void* b) {
+    if (*(int*)a > *(int*)b) return 1;
+    else return -1;
 }
 
-int arith(int list[], int n)
-{
-	double sum = 0;
+int arith(int list[], int n) {
+    double sum = 0;
 
-	for (int i = 0; i < n; i++)
-	{
-		sum += (list[i]);
-	}
+    for (int i = 0; i < n; i++) {
+        sum += (list[i]);
+    }
 
-	return round(sum / n);
+    return round(sum / n);
 }
 
-int median(int list[], int n)
-{
-	if (n == 1)
-		return list[0];
-	else
-		return list[(n + 1) / 2 - 1];
+int median(int list[], int n) {
+    if (n == 1) return list[0];
+    else return list[(n + 1) / 2 - 1];
 }
 
-int Mode(int list[], int n)
-{
-	int ar[8001] = { 0 };
-	int i, idx, max = 0, cnt = 0;
+int Mode(int list[], int n) {
+    int ar[8001] = { 0 };
+    int i, idx, max = 0, cnt = 0;
 
-	for (i = 0; i < n; i++)
-	{
-		idx = list[i] + 4000;
-		ar[idx] += 1;
-		
-		if (ar[idx] > max)
-			max = ar[idx];
-	}
+    for (i = 0; i < n; i++)
+    {
+        idx = list[i] + 4000;
+        ar[idx] += 1;
+        
+        if (ar[idx] > max)
+            max = ar[idx];
+    }
 
-	for (i = 0, idx = 0; i < 8001 ; i++)
-	{
-		if (ar[i] == 0)
-			continue;
+    for (i = 0, idx = 0; i < 8001 ; i++)
+    {
+        if (ar[i] == 0)
+            continue;
 
-		if (ar[i] == max)
-		{
-			if (cnt == 0)
-			{
-				idx = i;
-				cnt += 1;
-			}
-			else if (cnt == 1)
-			{
-				idx = i;
-				break;
-			}
-		}
-	}
-	return idx - 4000;
+        if (ar[i] == max)
+        {
+            if (cnt == 0)
+            {
+                idx = i;
+                cnt += 1;
+            }
+            else if (cnt == 1)
+            {
+                idx = i;
+                break;
+            }
+        }
+    }
+    return idx - 4000;
 
 }
 
-int range(int list[], int n)
-{
-	int max = list[n - 1];
-	int min = list[0];
+int range(int list[], int n) {
+    int max = list[n - 1];
+    int min = list[0];
 
-	return max - min;
+    return max - min;
 }
 
-int main()
-{
-	int i, n;
-	int* list;
+int main(void) {
+    int i, n;
+    int num[500000];
 
-	scanf("%d", &n);
-	list = (int*)calloc(n, sizeof(int));
+    scanf("%d", &n);
 
-	for (i = 0; i < n; i++)
-	{
-		scanf(" %d", &list[i]);
-	}
-	
-	qsort(list, n, sizeof(list[0]), compare);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &num[i]);
+    }
+    
+    qsort(num, n, sizeof(num[0]), compare);
 
-	printf("%d\n", arith(list, n));
-	printf("%d\n", median(list, n));
-	printf("%d\n", Mode(list, n));
-	printf("%d\n", range(list, n));
-	
-	return 0;
+    printf("%d\n", arith(num, n));
+    printf("%d\n", median(num, n));
+    printf("%d\n", Mode(num, n));
+    printf("%d\n", range(num, n));
+    
+    return 0;
 }
